faf00bfc0849efd14b2a6edb2244e89169a0447f
Johnny Vu <vujt@hotmail.com>
2020-05-29 16:56:33 -0700

    Remove unnecessary files

--- BethanysPieShopHRM.Server/Startup.cs
+++ BethanysPieShopHRM.Server/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BethanysPieShopHRM.Server.Data;
+
 
 namespace BethanysPieShopHRM.Server
 {
@@ -28,7 +28,7 @@ namespace BethanysPieShopHRM.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
